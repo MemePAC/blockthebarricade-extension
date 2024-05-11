81efcc7508f5a9946a6e8de2b04d547cac165ec8
Jackie Ni <jackieni2002@gmail.com>
2024-05-11 00:27:07 -0700

    added more figures and companies

--- scripts/content.js
+++ scripts/content.js
@@ -1,58 +1,80 @@
 chrome.storage.sync.get("blocking", (data) => {
-  if (data.blocking) {
-    const site = window.location.toString();
+  if (!data.blocking) {
+    return;
+  }
 
-    const blocklist = [
-      // companies
-      "https://www.mcdonalds.com",
-      "https://www.papajohns.com",
-      "https://www.dominos.com",
-      "https://www.bk.com",
+  const site = window.location.toString();
 
-      // public figures
-      "https://www.instagram.com/taylorswift",
-      "https://www.taylorswift.com",
-      "https://twitter.com/taylorswift13",
+  const blocklist = [
+    // companies
+    "https://www.mcdonalds.com",
+    "https://www.papajohns.com",
+    "https://www.dominos.com",
+    "https://www.bk.com",
+    "https://www.wix.com",
+    "https://www.pizzahut.com",
 
-      "https://www.amyschumer.com",
-      "https://www.instagram.com/amyschumer",
-      "https://twitter.com/amyschumer",
+    // public figures
+    "https://www.instagram.com/taylorswift",
+    "https://www.taylorswift.com",
+    "https://twitter.com/taylorswift13",
 
-      "https://www.instagram.com/noahschnapp",
-      "https://twitter.com/noah_schnapp",
-      "https://www.tiktok.com/@noahschnapp",
+    "https://www.amyschumer.com",
+    "https://www.instagram.com/amyschumer",
+    "https://twitter.com/amyschumer",
 
-      "https://www.instagram.com/kimkardashian",
-      "https://skknbykim.com",
-      "https://skims.com",
+    "https://www.instagram.com/noahschnapp",
+    "https://twitter.com/noah_schnapp",
+    "https://www.tiktok.com/@noahschnapp",
 
-      "https://www.charlieputh.com",
-      "https://www.instagram.com/charlieputh",
-      "https://twitter.com/charlieputh",
-    ];
+    "https://www.instagram.com/kimkardashian",
+    "https://skknbykim.com",
+    "https://skims.com",
 
-    if (blocklist.find((item) => site.includes(item))) {
-      window.location.href = "https://www.gofundme.com/s?q=palestine";
-    } else {
-      const links = document.getElementsByTagName("a");
+    "https://www.charlieputh.com",
+    "https://www.instagram.com/charlieputh",
+    "https://twitter.com/charlieputh",
 
-      for (let link of links) {
-        if (
-          link.textContent.toLowerCase().includes("drake") ||
-          link.textContent.toLowerCase().includes("kendrick")
-        ) {
-          link.href = "https://www.youtube.com/watch?v=fgDQyFeBBIo";
-          continue;
-        }
+    "https://www.instagram.com/sarahkatesilverman",
+    "https://twitter.com/SarahKSilverman",
 
-        if (
-          link.textContent.toLowerCase().includes("minouche") ||
-          link.textContent.toLowerCase().includes("shafik")
-        ) {
-          link.href = "https://www.instagram.com/presidentminouche/";
-          continue;
-        }
-      }
+    "https://www.instagram.com/missmayim",
+    "https://twitter.com/missmayim",
+
+    "https://twitter.com/Oprah",
+    "https://www.instagram.com/Oprah",
+
+    "https://twitter.com/elonmusk",
+    "https://www.instagram.com/elonmusk__.official",
+
+    "https://www.instagram.com/tchalamet",
+    "https://twitter.com/RealChalamet",
+
+    "https://www.instagram.com/trevornoah",
+    "https://twitter.com/Trevornoah",
+  ];
+
+  if (blocklist.find((item) => site.includes(item))) {
+    window.location.href = "https://www.gofundme.com/s?q=palestine";
+    return;
+  }
+
+  const links = document.getElementsByTagName("a");
+  for (let link of links) {
+    if (
+      link.textContent.toLowerCase().includes("drake") ||
+      link.textContent.toLowerCase().includes("kendrick")
+    ) {
+      link.href = "https://www.youtube.com/watch?v=fgDQyFeBBIo";
+      continue;
+    }
+
+    if (
+      link.textContent.toLowerCase().includes("minouche") ||
+      link.textContent.toLowerCase().includes("shafik")
+    ) {
+      link.href = "https://www.instagram.com/presidentminouche/";
+      continue;
     }
   }
 });
